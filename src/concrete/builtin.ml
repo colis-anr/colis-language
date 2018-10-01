88c28e51c96fcf44a8e27ede7b36a3dc75b3963d
Benedikt Becker <bbecker@inria.fr>
2018-10-01 15:34:42 +0200

    Continue concrete interpreter

--- src/concrete/builtin.ml
+++ src/concrete/builtin.ml
@@ -1,12 +1,11 @@
 open Semantics__Buffers
 open Semantics__Context
-open Mystring__String
 
 let interp_builtin : state -> string -> string list -> (state * stdout * bool) =
   fun sta name args ->
     match name with
     | "echo-n" ->
-      let str = concat args in
-      let out = output stdout empty_stdout |> newline in
+      let str = String.concat " " args in
+      let out = output str empty_stdout |> newline in
       sta, out, true
     | _ -> failwith ("interp_builtin: " ^ name)
--- src/concrete/concrete_colis.ml
+++ src/concrete/concrete_colis.ml
@@ -1,34 +1,27 @@
 open Format
-open Semantics__Behaviour
-open Interpreter__Result
+open Semantics__Context
+open Interpreter__Stdout
 open Interpreter__Interpreter
 
-let print_result fmt (res:Interpreter__Result.result) : unit =
-  faiwith "print_result"
+let get_parser = function
+  | "colis" -> FromColis.parse
+  | "shell" -> FromShell.parse
+  | _ ->
+    eprintf "Unknown file type: %s@." filetype;
+    exit 2
 
 let main () =
-  if Array.length Sys.argv != 1 then
-    exit 2;
-  let filename = Array.get Sys.argv 0 in
-
-  let point_pos = String.rindex filename '.' in
-
-  let ext = String.sub filename point_pos (String.length filename) in
-
-  let statement =
-    match ext with
-    | "cls" ->
-      FromColis.parse filename
-    | "sh" ->
-      FromShell.parse filename
-    | _ ->
-      Format.eprintf "Unknown extension: %s@." ext;
-      exit 2
+  if Array.length Sys.argv != 2 then begin
+    eprintf "Two arguments please: <filetype> <filename>, where <filename> is 'colis' or 'shell'";
+    exit 2
+  end;
+  let statement = get_parser filetype filename in
+  let out = stdout_new () in
+  let (ctx, sta, bool), mode =
+    try interp_stmt empty_input out empty_context empty_state statement, "normal"
+    with EExit res -> res, "exit"
   in
-  let res, success =
-    try interp_stmt empty_input empty_context empty_state statement, true
-    with EExit res -> res, false
-  in
-  printf "%s!@." (if success then "Normal" else "Exit");
-  printf "%a@." print_result res
+  printf "%s: %b@." mode b;
+  printf "STDOUT: %s@." (string_from_stdout !out)
 
+let () = main ()
