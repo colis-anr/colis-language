f40cbbe9d65f8891c9a80e20771b12b115197895
Benedikt Becker <bbecker@inria.fr>
2018-10-01 14:51:05 +0200

    Halfway concrete interpreter

--- src/concrete/builtin.ml
+++ src/concrete/builtin.ml
@@ -1,6 +1,12 @@
 open Semantics__Buffers
 open Semantics__Context
+open Mystring__String
 
 let interp_builtin : state -> string -> string list -> (state * stdout * bool) =
-  fun state name args ->
-    failwith "interp_builtin"
+  fun sta name args ->
+    match name with
+    | "echo-n" ->
+      let str = concat args in
+      let out = output stdout empty_stdout |> newline in
+      sta, out, true
+    | _ -> failwith ("interp_builtin: " ^ name)
