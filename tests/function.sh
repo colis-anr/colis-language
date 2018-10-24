7c61122013a990ab65abe96e60663c9b99932da6
Benedikt Becker <benedikt.becker@inria.fr>
2018-10-24 13:50:49 +0200

    Prettier function test

--- tests/function.sh
+++ tests/function.sh
@@ -1,10 +1,20 @@
-f () {
+greet () {
   echo "Hello" "$1!"
 }
 
 twice () {
-  f $1
-  f $2
+  echo Function: $0
+  greet $1
+  greet $2
 }
 
+butfirst () {
+    shift
+    echo Function: $0
+    greet $1
+    greet $2
+}
+
+echo Program: $0
 twice 'colis' 'world'
+butfirst 'you' 'colis' 'world'
