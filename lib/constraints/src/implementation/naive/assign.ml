227ba8e4e756adf38d7b5146be1000aa8fb9a041
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-11-26 16:23:11 +0100

    assignments are now maps and not lists

--- lib/constraints/src/implementation/naive/assign.ml
+++ lib/constraints/src/implementation/naive/assign.ml
@@ -1,46 +1,49 @@
 open Constraints_common
 
-type t = (* invariant: always sorted *)
-  { vars : (Metavar.t * Var.t) list ;
-    feats : (Metavar.t * Feat.t) list ;
-    feat_sets : (Metavar.t * Feat.Set.t) list }
+type t =
+  { vars : Var.t Metavar.Map.t ;
+    feats : Feat.t Metavar.Map.t ;
+    feat_sets : Feat.Set.t Metavar.Map.t }
 
-let var aff m = List.assoc m aff.vars
-let feat aff m = List.assoc m aff.feats
-let feat_set aff m = List.assoc m aff.feat_sets
+let var aff m = Metavar.Map.find m aff.vars
+let feat aff m = Metavar.Map.find m aff.feats
+let feat_set aff m = Metavar.Map.find m aff.feat_sets
 
-let empty = { vars = [] ; feats = [] ; feat_sets = [] }
-
-let sort l = List.sort (fun (m, _) (n, _) -> Metavar.compare m n) l
+let empty =
+  { vars = Metavar.Map.empty ;
+    feats = Metavar.Map.empty ;
+    feat_sets = Metavar.Map.empty }
 
 let from_lists ?(vars=[]) ?(feats=[]) ?(feat_sets=[]) () =
-  { vars = sort vars ;
-    feats = sort feats ;
-    feat_sets = sort feat_sets }
+  let rec map_add_list l m =
+    match l with
+    | [] -> m
+    | (hk, hv) :: t -> map_add_list t (Metavar.Map.add hk hv m)
+  in
+  { vars = map_add_list vars Metavar.Map.empty ;
+    feats = map_add_list feats Metavar.Map.empty ;
+    feat_sets = map_add_list feat_sets Metavar.Map.empty }
 
 let (>>=) = OptionMonad.(>>=)
 
-let simplify equal subaff =
-  let open OptionMonad in
-  let rec simplify = function
-    | [] -> Some []
-    | [a] -> Some [a]
-    | (m, x) :: (n, y) :: subaff ->
-       if Metavar.equal m n then
-         if equal x y then
-           simplify ((n, y) :: subaff)
-         else
-           None
-       else
-         simplify ((n, y) :: subaff) >>= fun subaffect ->
-         Some ((m, x) :: subaffect)
-  in
-  simplify subaff
+let map_distinct_union equal m1 m2 =
+  try
+    Some (
+        Metavar.Map.union
+          (fun _ v1 v2 ->
+            if equal v1 v2 then
+              Some v1
+            else
+              raise (Invalid_argument "map_distinct_union"))
+          m1 m2
+      )
+  with
+    Invalid_argument _ -> None
 
 let merge aff1 aff2 =
-  (aff1.vars @ aff2.vars) |> sort |> simplify Var.equal >>= fun vars ->
-  (aff1.feats @ aff2.feats) |> sort |> simplify Feat.equal >>= fun feats ->
-  (aff1.feat_sets @ aff2.feat_sets) |> sort |> simplify Feat.Set.equal >>= fun feat_sets ->
+  map_distinct_union Var.equal aff1.vars aff2.vars >>= fun vars ->
+  map_distinct_union Feat.equal aff1.feats aff2.feats >>= fun feats ->
+  map_distinct_union Feat.Set.equal aff1.feat_sets aff2.feat_sets >>= fun feat_sets ->
   Some { vars ; feats ; feat_sets }
 
 let compatible aff1 aff2 =
