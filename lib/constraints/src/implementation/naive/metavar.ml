47d29bf86ba8306737e080f8a2bd791e8b45031f
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-11-26 16:23:11 +0100

    assignments are now maps and not lists

--- lib/constraints/src/implementation/naive/metavar.ml
+++ lib/constraints/src/implementation/naive/metavar.ml
@@ -11,3 +11,12 @@ let fresh5 () = (fresh (), fresh (), fresh (), fresh (), fresh ())
 
 let compare = compare
 let equal m n = compare m n = 0
+
+module Self = struct
+  type s = t
+  type t = s
+
+  let compare = compare
+end
+
+module Map = Map.Make(Self)
