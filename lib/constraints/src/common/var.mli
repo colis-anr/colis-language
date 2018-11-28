6ed3076941a0660b951b2015eea624bf52a83f3a
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-11-28 17:52:10 +0100

    rename Var.show -> Var.to_string

--- lib/constraints/src/common/var.mli
+++ lib/constraints/src/common/var.mli
@@ -5,7 +5,7 @@ val fresh : ?hint:string -> unit -> t
 val compare : t -> t -> int
 val equal : t -> t -> bool
 
-val show : t -> string
+val to_string : t -> string
 val pp : Format.formatter -> t -> unit
 
 module Set : sig
