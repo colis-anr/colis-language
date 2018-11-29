b7db7cbdff1a0df56fbb3219acb50efd4b02f88e
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-11-29 12:25:06 +0100

    reg,dir -> kinds, rules for kinds

--- lib/constraints/src/implementation/naive/assign.ml
+++ lib/constraints/src/implementation/naive/assign.ml
@@ -3,18 +3,21 @@ open Constraints_common
 type t =
   { vars : Var.t Metavar.Map.t ;
     feats : Feat.t Metavar.Map.t ;
+    kinds : Kind.t Metavar.Map.t ;
     feat_sets : Feat.Set.t Metavar.Map.t }
 
 let var aff m = Metavar.Map.find m aff.vars
 let feat aff m = Metavar.Map.find m aff.feats
+let kind aff m = Metavar.Map.find m aff.kinds
 let feat_set aff m = Metavar.Map.find m aff.feat_sets
 
 let empty =
   { vars = Metavar.Map.empty ;
     feats = Metavar.Map.empty ;
+    kinds = Metavar.Map.empty ;
     feat_sets = Metavar.Map.empty }
 
-let from_lists ?(vars=[]) ?(feats=[]) ?(feat_sets=[]) () =
+let from_lists ?(vars=[]) ?(feats=[]) ?(kinds=[]) ?(feat_sets=[]) () =
   let rec map_add_list l m =
     match l with
     | [] -> m
@@ -22,6 +25,7 @@ let from_lists ?(vars=[]) ?(feats=[]) ?(feat_sets=[]) () =
   in
   { vars = map_add_list vars Metavar.Map.empty ;
     feats = map_add_list feats Metavar.Map.empty ;
+    kinds = map_add_list kinds Metavar.Map.empty ;
     feat_sets = map_add_list feat_sets Metavar.Map.empty }
 
 let (>>=) = OptionMonad.(>>=)
@@ -43,8 +47,9 @@ let map_distinct_union equal m1 m2 =
 let merge aff1 aff2 =
   map_distinct_union Var.equal aff1.vars aff2.vars >>= fun vars ->
   map_distinct_union Feat.equal aff1.feats aff2.feats >>= fun feats ->
+  map_distinct_union Kind.equal aff1.kinds aff2.kinds >>= fun kinds ->
   map_distinct_union Feat.Set.equal aff1.feat_sets aff2.feat_sets >>= fun feat_sets ->
-  Some { vars ; feats ; feat_sets }
+  Some { vars ; feats ; kinds ; feat_sets }
 
 let compatible aff1 aff2 =
   merge aff1 aff2 <> None
