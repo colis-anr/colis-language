2277ee1f43f0995a3d47c4dd260c5c5ab543d4c1
Niols <niols@niols.fr>
2018-12-03 11:08:43 +0100

    move sets pretty-printing away from the definitions of the sets themselves

--- lib/constraints/src/common/conj.ml
+++ lib/constraints/src/common/conj.ml
@@ -2,9 +2,19 @@ type t = Var.Set.t * Literal.Set.t
 [@@deriving eq, ord]
 
 let pp fmt (es, c) =
-  if not (Var.Set.is_empty es) then
-    Format.fprintf fmt "∃ %a⋅ " Var.Set.pp es;
-  if Literal.Set.is_empty c then
-    Format.pp_print_string fmt "⊤"
-  else
-    Literal.Set.pp fmt c
+  let fpf = Format.fprintf in
+  (
+    match Var.Set.elements es with
+    | [] -> ()
+    | v :: vs ->
+       fpf fmt "∃ %a" Var.pp v;
+       List.iter (fpf fmt ", %a" Var.pp) vs
+  );
+  (
+    match Literal.Set.elements c with
+    | [] ->
+       fpf fmt "⊤"
+    | l :: ls ->
+       Literal.pp fmt l;
+       List.iter (fpf fmt " ∧ %a" Literal.pp) ls
+  )
--- lib/constraints/src/common/literal.ml
+++ lib/constraints/src/common/literal.ml
@@ -1,5 +1,3 @@
-let fpf = Format.fprintf
-
 type t =
   | Pos of Atom.t
   | Neg of Atom.t
@@ -7,7 +5,7 @@ type t =
 
 let pp fmt = function
   | Pos a -> Atom.pp fmt a
-  | Neg a -> fpf fmt "¬ %a" Atom.pp a
+  | Neg a -> Format.fprintf fmt "¬ %a" Atom.pp a
 
 module Self = struct
   type s = t
@@ -17,12 +15,4 @@ module Self = struct
   let equal = equal
 end
 
-module Set = struct
-  include Set.Make(Self)
-
-  let pp fmt fs =
-    match elements fs with
-    | [] -> ()
-    | f :: fs -> pp fmt f;
-                 List.iter (fpf fmt " ∧ %a" pp) fs
-end
+module Set = Set.Make(Self)
