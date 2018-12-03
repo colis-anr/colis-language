bd5c6e1d8509539607f76c862b891942b2b7f37e
Niols <niols@niols.fr>
2018-12-03 10:57:35 +0100

    move Conj from Implementation/Naive to Common

--- lib/constraints/src/implementation/naive/rules.mli
+++ lib/constraints/src/implementation/naive/rules.mli
@@ -2,4 +2,4 @@ open Constraints_common
 
 val accessibility : Literal.Set.t -> (Var.t * Var.Set.t) list
 
-val all : (string * (Conj.t -> Conj.disj option)) list
+val all : (string * (Conj.t -> Disj.t option)) list
