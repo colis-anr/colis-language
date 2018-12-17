258779be4c07f3c187f9d8f6839527901c4d5c21
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-12-17 15:52:37 +0100

    more on dot

--- lib/constraints/src/common/conj.ml
+++ lib/constraints/src/common/conj.ml
@@ -21,33 +21,67 @@ let pp fmt (es, c) =
        List.iter (fpf fmt " ∧ %a" Literal.pp) ls
   )
 
+let vars (es, c) =
+  let us = Literal.Set.fold (fun l xs -> Var.Set.union (Literal.vars l) xs) c Var.Set.empty in
+  Var.Set.union es us
+
 let pp_as_dot ~name fmt (es, c) =
-  let invis = let invis = ref 0 in fun () -> incr invis; !invis in
+  let pp_literal_no_var fmt = function
+    | Literal.Pos (Eq _) -> fpf fmt "="
+    | Neg (Eq _) -> fpf fmt "≠"
+    | Pos (Abs (_, f)) -> fpf fmt "[%a]↑" Feat.pp f
+    | Pos (Fen (_, fs)) -> fpf fmt "[%a]" Feat.Set.pp fs
+    | Neg (Fen (_, fs)) -> fpf fmt "¬[%a]" Feat.Set.pp fs
+    | Pos (Sim (_, fs, _)) -> fpf fmt "∼%a" Feat.Set.pp fs
+    | Neg (Sim (_, fs, _)) -> fpf fmt "≁%a" Feat.Set.pp fs
+    | _ -> failwith "pp_literal_no_var"
+  in
+
+  let is_atom_unary_about_var x = function
+    | Atom.Eq _ | Feat _ | Sim _ -> false
+    | Abs (y, _) | Kind (y, _) | Fen (y, _) -> Var.equal x y
+  in
+
+  let is_literal_unary_about_var x = function
+    | Literal.Pos a | Neg a -> is_atom_unary_about_var x a
+  in
+
+  let pp_var_unary_attributes fmt x =
+    Literal.Set.iter
+      (fun l ->
+        if is_literal_unary_about_var x l then
+          fpf fmt " | %a " pp_literal_no_var l)
+      c
+  in
+
+  let hash = Hashtbl.hash in
 
   fpf fmt "@[<h 2>digraph %s {@\n" name;
 
-  (* Print nodes. Circle for existentially quantified, square for
-     others. *)
+  (* Print nodes and unary literals. *)
   Var.Set.iter
-    (fun v ->
-      fpf fmt "\"%a\" [shape=circle];@\n" Var.pp v)
-    es;
-  fpf fmt "node [shape=square];@\n";
+    (fun x ->
+      fpf fmt "%d [shape=record,label=\"%s%a%a\"];@\n"
+        (hash x)
+        (if Var.Set.mem x es then "∃" else "")
+        Var.pp x pp_var_unary_attributes x)
+    (vars (es, c));
 
-  (* Print literals. *)
+  (* Print binary literals *)
   Literal.Set.iter
     (function
      | Pos (Feat (x, f, y)) ->
-        fpf fmt "\"%a\" -> \"%a\" [label=\"%a\"];@\n" Var.pp x Var.pp y Feat.pp f
-     | Pos (Abs (x, f)) ->
-        let i = invis () in
-        fpf fmt "\"%d\" [shape=point, style=invis];@\n" i;
-        fpf fmt "\"%a\" -> \"%d\" [label=\"%a\", arrowhead=box];@\n" Var.pp x i Feat.pp f
+        fpf fmt "%d -> %d [label=\"%a\"];@\n" (hash x) (hash y) Feat.pp f
+     | Neg (Feat (x, f, y)) ->
+        fpf fmt "%d -> %d [label=\"¬%a\", style=dashed, constraint=false];@\n"
+          (hash x) (hash y) Feat.pp f
      | Pos (Sim (x, fs, y)) ->
-        fpf fmt "\"%a\" -> \"%a\" [label=\"~%a\", style=dashed, arrowhead=none, constraint=false];@\n" Var.pp x Var.pp y Feat.Set.pp fs
+        fpf fmt "%d -> %d [label=\"∼%a\", style=dashed, arrowhead=none, constraint=false];@\n"
+          (hash x) (hash y) Feat.Set.pp fs
      | Neg (Sim (x, fs, y)) ->
-        fpf fmt "\"%a\" -> \"%a\" [label=\"¬~%a\", style=dashed, arrowhead=none, constraint=false];@\n" Var.pp x Var.pp y Feat.Set.pp fs
-     | l ->
-        Log.err (fun m -> m "Conj.pp_as_dot: unsupported literal (%a)" Literal.pp l))
+        fpf fmt "%d -> %d [label=\"≁%a\", style=dashed, arrowhead=none, constraint=false];@\n"
+          (hash x) (hash y) Feat.Set.pp fs
+     | _ ->
+        ())
     c;
   fpf fmt "}@]"
