0d7c28871bed4a2a7ab6faa97c46479d8e11b098
Benedikt Becker <benedikt.becker@inria.fr>
2019-01-24 15:23:45 +0100

    Fix doc strings in colis.mli

--- src/colis.mli
+++ src/colis.mli
@@ -46,23 +46,23 @@ type shell = Morsmall.AST.program
 val colis_from_channel : ?filename:string -> in_channel -> colis
 (** Reads Colis syntax from a channel and returns the corresponding AST.
 
-    @raises {!Errors.ParseError} *)
+    @raise {!Errors.ParseError} *)
 
 val colis_from_file : string -> colis
 (** Reads Colis syntax from a file and returns the corresponding AST.
 
-    @raises {!Errors.ParseError} *)
+    @raise {!Errors.ParseError} *)
 
 val colis_from_string : string -> colis
 (** Reads Colis syntax from a string and returns the corresponding AST.
 
-    @raises {!Errors.ParseError} *)
+    @raise {!Errors.ParseError} *)
 
 val shell_from_file : string -> shell
 (** Reads a Shell file and returns the corresponding AST. This is a
     wrapper around Morsmall.parse_file.
 
-    @raises {!Errors.ParseError} *)
+    @raise {!Errors.ParseError} *)
 
 (** {2 Printing} *)
 
@@ -83,7 +83,7 @@ val pp_print_colis : Format.formatter -> colis -> unit
 val shell_to_colis : shell -> colis
 (** Converts a Shell program to a Colis program.
 
-    @raises {!Errors.ConversionError} *)
+    @raise {!Errors.ConversionError} *)
 
 (** {2 Interpreting} *)
 
