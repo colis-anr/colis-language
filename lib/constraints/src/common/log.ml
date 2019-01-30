3f121a680602fdba78c801178eb4ad2185b7baa8
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2019-01-30 14:52:50 +0100

    loglevel = info by default

--- lib/constraints/src/common/log.ml
+++ lib/constraints/src/common/log.ml
@@ -1,8 +1,30 @@
-let log level f =
-  Format.eprintf "[%s]" level;
-  f Format.eprintf;
-  Format.eprintf "@."
+module Loglevel = struct
+  type t =
+    | Debug
+    | Info
+    | Warning
 
-let debug f = log "DEBUG" f
-let info f = log "INFO" f
-let warn f = log "WARN" f
+  let to_string = function
+    | Debug -> "DEBUG"
+    | Info -> "INFO"
+    | Warning -> "WARNING"
+
+  let to_int = function
+    | Debug -> 0
+    | Info -> 1
+    | Warning -> 2
+end
+
+let current = ref Loglevel.Info
+
+let log level f =
+  if Loglevel.to_int level >= Loglevel.to_int !current then
+    (
+      Format.eprintf "[%s]" (Loglevel.to_string level);
+      f Format.eprintf;
+      Format.eprintf "@."
+    )
+
+let debug f = log Debug f
+let info f = log Info f
+let warn f = log Warning f
