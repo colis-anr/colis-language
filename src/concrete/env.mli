b16f29e119974d51dff64c071a9a575131130905
Benedikt Becker <benedikt.becker@inria.fr>
2019-03-27 18:18:28 +0100

    Implement null values in variable environment

--- src/concrete/env.mli
+++ src/concrete/env.mli
@@ -5,5 +5,5 @@ val empty : 'a -> 'a env
 val get : 'a env -> string -> 'a
 val set : 'a env -> string -> 'a -> 'a env
 val filter : (string -> 'a -> bool) -> 'a env -> 'a env
-val map : ('a -> 'b) -> 'a env -> 'b env
+val map : ('a -> 'b) -> 'b -> 'a env -> 'b env
 val elements : 'a env -> (string * 'a) Seq.t
