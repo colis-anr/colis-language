5f3d66f36ab5846a99a21ed49deea966a561a48a
Benedikt Becker <benedikt.becker@inria.fr>
2019-03-27 11:09:16 +0100

    Add tests for export

---
+++ tests/export.sh
@@ -0,0 +1,15 @@
+echo 1
+env|grep COLIS_X || true
+COLIS_X=x
+echo 2
+env|grep COLIS_X || true
+export COLIS_X
+echo 3
+env|grep COLIS_X || true
+
+export COLIS_Y
+echo 4
+env|grep COLIS_Y || true
+COLIS_Y=y
+echo 5
+env|grep COLIS_Y || true
