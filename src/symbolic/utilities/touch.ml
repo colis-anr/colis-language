49266ccc5d9918d5650b74a61d51f00eeeb19b44
Niols <niols@niols.fr>
2019-07-19 09:23:32 +0200

    use maybe_resolve in touch

--- src/symbolic/utilities/touch.ml
+++ src/symbolic/utilities/touch.ml
@@ -13,55 +13,52 @@ let interp_touch1 cwd path_str : utility =
   match Path.split_last p with
   | None -> (* `touch ''` *)
     failure ~error_message:"cannot touch '': No such file or directory" ()
-  | Some (q, comp) ->
-    let common_cases =
-      let hint = last_comp_as_hint ~root p in [
-        success_case
-          ~descr:(asprintf "touch %a: path resolves" Path.pp p)
-          begin
-            exists ?hint @@ fun y ->
-            resolve root cwd p y &
-            eq root root'
-          end;
-        error_case
-          ~descr:(asprintf "touch %a: parent path does not resolve" Path.pp p)
-          begin
-            noresolve root cwd q &
-            eq root root';
-          end
-      ]
-    in
-    let feature_cases f =
-      let hintx = last_comp_as_hint ~root q in
-      let hinty = Feat.to_string f in [
-        success_case
-          ~descr:(asprintf "touch %a: create file" Path.pp p)
-          begin
-            exists3 ?hint1:hintx ?hint2:hintx ~hint3:hinty @@ fun x x' y' ->
-            resolve root cwd q x &
-            dir x &
-            abs x f &
-            similar root root' cwd q x x' &
-            sim x (Feat.Set.singleton f) x' &
-            dir x' &
-            feat x' f y' &
-            reg y'
-          end;
-        error_case
-          ~descr:(asprintf "touch %a: parent path is not directory" Path.pp p)
-          begin
-            exists ?hint:hintx @@ fun x ->
-            resolve root cwd q x &
-            ndir x &
-            eq root root'
-          end
-      ]
-    in
-    match comp with
-    | Up | Here ->
-      common_cases
-    | Down f ->
-      common_cases @ feature_cases f
+  | Some (q, (Up | Here)) ->
+    let hint = last_comp_as_hint ~root p in [
+      success_case
+        ~descr:(asprintf "touch %a: path resolves" Path.pp p)
+        begin
+          exists ?hint @@ fun y ->
+          resolve root cwd p y &
+          eq root root'
+        end;
+      error_case
+        ~descr:(asprintf "touch %a: path does not resolve" Path.pp p)
+        begin
+          noresolve root cwd p &
+          eq root root'
+        end
+    ]
+  | Some (q, Down f) ->
+    let hintx = last_comp_as_hint ~root q in
+    let hinty = Feat.to_string f in [
+      success_case
+        ~descr:(asprintf "touch %a: path resolves" Path.pp p)
+        begin
+          exists ~hint:hinty @@ fun y ->
+          resolve root cwd p y &
+          eq root root'
+        end;
+      success_case
+        ~descr:(asprintf "touch %a: create file" Path.pp p)
+        begin
+          exists3 ?hint1:hintx ?hint2:hintx ~hint3:hinty @@ fun x x' y' ->
+          resolve root cwd q x &
+          dir x &
+          abs x f &
+          similar root root' cwd q x x' &
+          sim x (Feat.Set.singleton f) x' &
+          dir x' &
+          feat x' f y' &
+          reg y'
+        end;
+      error_case
+        ~descr:(asprintf "touch %a: parent path does not resolve or resolves to dir" Path.pp p)
+        begin
+          maybe_resolve root cwd q (fun z -> ndir z)
+          & eq root root'
+        end
+    ]
 
 let interprete ctx args =
   multiple_times (interp_touch1 ctx.cwd) args
