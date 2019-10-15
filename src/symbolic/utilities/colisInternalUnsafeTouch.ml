4ef2b6dd900880dd2c4d07dbbb48bc14a1ab1114
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2019-10-15 18:37:18 +0200

    colis internal unsafe touch, untested yet

---
+++ src/symbolic/utilities/colisInternalUnsafeTouch.ml
@@ -0,0 +1,33 @@
+open Format
+open Colis_constraints
+open Clause
+open SymbolicUtility
+
+let name = "colis_internal_unsafe_touch"
+
+let interp1 path_str =
+  under_specifications @@ fun ~root ~root' ->
+  let p = Path.from_string path_str in
+  let q = Path.check_normal p in
+  [
+    success_case
+      ~descr:(asprintf "colis_internal_unsafe_touch: %a" Path.pp p)
+      begin
+        let rec aux x x' = function
+          | [] ->
+            reg x'
+          | f :: q ->
+            exists2 @@ fun y y' ->
+            sim x (Feat.Set.singleton f) x'
+            & maybe x f y & feat x' f y'
+            & aux y y' q
+        in
+        aux root root' q
+      end
+  ]
+
+let interprete _ctx args : utility =
+  multiple_times interp1 args
+
+let interprete ctx : utility =
+  cmdliner_eval_utility ~utility:name Cmdliner.Term.(const interprete) ctx
