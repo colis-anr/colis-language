a660992b8c4508607de864e26c0842c05f77de75
Ralf Treinen <treinen@irif.fr>
2019-12-09 11:32:43 +0100

    add is_dir_prefix to module Pathnames

--- src/concrete/pathnames.ml
+++ src/concrete/pathnames.ml
@@ -8,4 +8,20 @@ let remove_dirs s =
     in String.sub s (last_slash_ind+1) ((String.length s)-last_slash_ind-1)
   with
     Not_found -> s
-;;
+
+let is_dir_prefix p1 p2 =
+  (* check whether [p1^'/'] is a prefix of [p2] *)
+  let rec forall_from_to lower upper pred =
+    (* check [(pred lower) && .... && (pred upper)] *)
+    if lower > upper then true
+    else pred lower && forall_from_to (lower+1) upper pred
+  in
+  let n1 = String.length p1
+  and n2 = String.length p2
+  in
+  if n1+1 >= n2
+  then
+    false
+  else
+    p2.[n1]='/' && forall_from_to 0 (n1-1) (function i -> p1.[i]=p2.[i])
+
--- src/concrete/pathnames.mli
+++ src/concrete/pathnames.mli
@@ -1,10 +1,13 @@
-(* operations on path names *)
+(** operations on path names *)
 
-(* check whether the first symbol of the path name is the symbol '/' *)
+(** check whether the first symbol of the path name is the symbol '/' *)
 val starts_on_slash: string -> bool
 
-(* check whether the last symbol of the path name is the symbol '/' *)
+(** check whether the last symbol of the path name is the symbol '/' *)
 val ends_on_slash: string -> bool
 
-(* return the path name with any leading directory components removed *)
+(** return the path name with any leading directory components removed *)
 val remove_dirs: string -> string
+
+(** [is_pathprefix s1 s2] checks whether [s1^"/"] is a prefix of [s2] *)
+val is_dir_prefix: string -> string -> bool
