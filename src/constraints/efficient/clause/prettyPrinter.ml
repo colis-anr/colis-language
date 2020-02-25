1e02f1a46f0f0b18594d4132fa3c5a138beb3935
Niols <niols@niols.fr>
2020-02-25 13:39:31 +0100

    constraints.efficient: split pp_as_dot

--- src/constraints/efficient/clause/prettyPrinter.ml
+++ src/constraints/efficient/clause/prettyPrinter.ml
@@ -3,9 +3,9 @@ let fpf = Format.fprintf
 
 let pp = Core.pp_debug
 
-let pp_as_dot ~name fmt c =
-  let c = Core.simplify c in
-  let fresh = let c = ref 0 in fun () -> incr c; "fresh_" ^ string_of_int !c in
+module Dot = struct
+  let fresh = let c = ref 0 in fun () -> incr c; "fresh_" ^ string_of_int !c
+
   let pp_node fmt ?text x =
     Format.kasprintf
       (fun cont ->
@@ -21,13 +21,12 @@ let pp_as_dot ~name fmt c =
            fpf fmt "%d [label=< <TABLE BORDER=\"0\" CELLBORDER=\"1\" CELLSPACING=\"0\"><TR><TD></TD></TR></TABLE> >];@\n"
              (Core.hash x))
       "%a"
-  in
+
   let pp_flat_edge fmt x y = (* assumes x and y are hashed *)
     (* let x = Core.hash x in
-    let y = Core.hash y in *)
+       let y = Core.hash y in *)
     let (x, y) = if x < y then (x, y) else (y, x) in
     fpf fmt "{ rank=same; %d -> %d [style=dotted,arrowhead=none,label=\"%a\"]; }@\n" x y
-  in
   let pp_kind_and_fen fmt info =
     (match Core.get_kind info with
      | Any -> ()
@@ -39,8 +38,8 @@ let pp_as_dot ~name fmt c =
        fpf fmt "<TR><TD>%a</TD></TR>" Kind.pp kind);
     if Core.has_fen info then
       fpf fmt "<TR><TD>full</TD></TR>"
-  in
-  let pp_feats fmt x info_x =
+
+  let pp_feats fmt c x info_x =
     if not (Core.is_shadow x c) then
       Core.iter_feats
         (fun f -> function
@@ -62,8 +61,8 @@ let pp_as_dot ~name fmt c =
                ys
         )
         info_x
-  in
-  let pp_sims fmt x info_x =
+
+  let pp_sims fmt c x info_x =
     let hx = Core.hash x in
     Core.iter_sims
       (fun fs y ->
@@ -72,43 +71,48 @@ let pp_as_dot ~name fmt c =
          if hx < hy && not (Core.is_shadow x c) && not (Core.is_shadow y c) then
            pp_flat_edge fmt hx hy (fun fmt -> fpf fmt "~%a" Feat.Set.pp) fs)
       info_x
-  in
-  let pp fmt c =
-    Core.iter_infos
-      (fun x info_x ->
-         if not (Core.is_shadow x c) then
+
+  let pp ~name fmt c =
+    let c = Core.simplify c in
+    let pp fmt c =
+      Core.iter_infos
+        (fun x info_x ->
+           if not (Core.is_shadow x c) then
+             let text =
+               match Core.externalise x c with
+               | [] -> None
+               | xs ->
+                 xs
+                 |> List.sort Var.compare
+                 |> List.map Var.to_string
+                 |> String.concat ", "
+                 |> (fun s -> Some s)
+             in
+             pp_node fmt ?text x
+               pp_kind_and_fen info_x)
+        c;
+      fpf fmt "@\n";
+      Core.iter_equalities
+        (fun x y ->
            let text =
              match Core.externalise x c with
              | [] -> None
              | xs ->
                xs
-               |> List.sort Var.compare
                |> List.map Var.to_string
                |> String.concat ", "
                |> (fun s -> Some s)
            in
            pp_node fmt ?text x
-             pp_kind_and_fen info_x)
-      c;
-    fpf fmt "@\n";
-    Core.iter_equalities
-      (fun x y ->
-         let text =
-           match Core.externalise x c with
-           | [] -> None
-           | xs ->
-             xs
-             |> List.map Var.to_string
-             |> String.concat ", "
-             |> (fun s -> Some s)
-         in
-         pp_node fmt ?text x
-           (fun _fmt () -> ()) ();
-         pp_flat_edge fmt (Core.hash x) (Core.hash y) (fun fmt () -> fpf fmt "=") ())
-      c;
-    fpf fmt "@\n";
-    Core.iter_infos (pp_feats fmt) c;
-    fpf fmt "@\n";
-    Core.iter_infos (pp_sims fmt) c
-  in
-  fpf fmt "@[<h 2>digraph %S {@\nnode [shape=plaintext];@\nedge [arrowhead=none];@\n@\n%a@]@\n}" name pp c
+             (fun _fmt () -> ()) ();
+           pp_flat_edge fmt (Core.hash x) (Core.hash y) (fun fmt () -> fpf fmt "=") ())
+        c;
+      fpf fmt "@\n";
+      Core.iter_infos (pp_feats fmt c) c;
+      fpf fmt "@\n";
+      Core.iter_infos (pp_sims fmt c) c
+    in
+    fpf fmt "@[<h 2>digraph %S {@\nnode [shape=plaintext];@\nedge [arrowhead=none];@\n@\n%a@]@\n}" name pp c
+end
+
+let pp_as_dot = Dot.pp
